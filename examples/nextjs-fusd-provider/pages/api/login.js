a587c32d85160181b17bfeccb9935b03978197e2
kurodenjiro <kurodenjiro1@gmail.com>
2023-02-23 05:37:17 +0700

    update

---
+++ examples/nextjs-fusd-provider/pages/api/login.js
@@ -0,0 +1,30 @@
+import WalletApiClient from "../../lib/walletApi";
+import {adminAddress, baseUrl, fusdTokenName } from "../../lib/config"
+
+const walletApi = new WalletApiClient(baseUrl)
+
+export default async function handler(req, res) {
+  if (req.method === "GET") {
+    return get(req, res)
+  }
+
+  if (req.method === "POST") {
+    return post(req, res)
+  }
+}
+
+// Login
+async function post(req, res) {
+    const { username, password } = req.body
+  const result = await walletApi.loginAccount(username,password)
+
+  res.status(200).json({
+    "message": "Login success",
+    "error": false,
+    data:result
+  })
+}
+
+const emptyBalance = "0.00000000"
+
+
