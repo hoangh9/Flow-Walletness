99a02f2f1bf148c47c1947143745256cef18dd84
kurodenjiro <kurodenjiro1@gmail.com>
2023-03-01 03:27:25 +0700

    update

---
+++ examples/nextjs-fusd-provider/pages/api/history.js
@@ -0,0 +1,28 @@
+import WalletApiClient from "../../lib/walletApi";
+import {adminAddress, baseUrl, fusdTokenName } from "../../lib/config"
+
+const walletApi = new WalletApiClient(baseUrl)
+
+export default async function handler(req, res) {
+  if (req.method === "GET") {
+    return get(req, res)
+  }
+
+  if (req.method === "POST") {
+    return post(req, res)
+  }
+}
+
+// list accounts
+async function get(req, res) {
+    const listTokenDeposits = await walletApi.getTokenDeposits(req.query.address,fusdTokenName);
+    const listTokenWithdrawals = await walletApi.getTokenwithdrawals(req.query.address,fusdTokenName);
+    const data = listTokenDeposits.concat(listTokenWithdrawals).sort((a, b) => a.createdAt - b.createdAt);
+    console.log(data);
+      return res.status(200).json({
+        "message": "success",
+        "error": false,
+        data:data
+      })
+}
+
