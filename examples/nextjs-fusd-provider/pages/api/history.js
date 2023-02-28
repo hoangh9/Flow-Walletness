372cd9096eed0efa29a60e2d6a8c81a59737f532
kurodenjiro <kurodenjiro1@gmail.com>
2023-03-01 03:36:48 +0700

    update

--- examples/nextjs-fusd-provider/pages/api/history.js
+++ examples/nextjs-fusd-provider/pages/api/history.js
@@ -16,7 +16,13 @@ export default async function handler(req, res) {
 // list accounts
 async function get(req, res) {
     const listTokenDeposits = await walletApi.getTokenDeposits(req.query.address,fusdTokenName);
+    listTokenDeposits.forEach(item => {
+      item.push({type:"income"})
+    });
     const listTokenWithdrawals = await walletApi.getTokenwithdrawals(req.query.address,fusdTokenName);
+    listTokenWithdrawals.forEach(item => {
+      item.push({type:"expense"})
+    });
     const data = listTokenDeposits.concat(listTokenWithdrawals).sort((a, b) => a.createdAt - b.createdAt);
     console.log(data);
       return res.status(200).json({
