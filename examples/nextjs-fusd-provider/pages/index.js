1d9fa79c8ebfc5ef77d0bb993e567f907b284852
kurodenjiro <kurodenjiro1@gmail.com>
2023-02-28 23:02:46 +0700

    update

--- examples/nextjs-fusd-provider/pages/index.js
+++ examples/nextjs-fusd-provider/pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
 
   const syncAccounts = async () => {
     const accounts = await listAccounts()
-    setAccounts(accounts)
+    setAccounts(accounts.data)
   }
 
   const addAccount = account => {
